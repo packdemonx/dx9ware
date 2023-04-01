c3a582dff45c634fb30afd0d279b785ced390252
packdemonx <125107593+packdemonx@users.noreply.github.com>
2023-04-01 19:07:45 -0400

    Add files via upload

---
+++ dx9 GUI injector Cracked  ROBLOX !.lua
@@ -0,0 +1,75 @@
+-- DX9 INJECTOR CR4CKED
+-- Version: 1.0
+
+-- Instances:
+
+local loaderexe = Instance.new("ScreenGui")
+local D = Instance.new("Frame")
+local X = Instance.new("ImageLabel")
+local Ava81ng81na89fnqa812nva = Instance.new("TextButton")
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local ImageLabel = Instance.new("ImageLabel")
+
+--Properties:
+
+loaderexe.Name = "loader.exe"
+loaderexe.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+D.Name = "D"
+D.Parent = loaderexe
+D.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+D.Position = UDim2.new(-0.000535070896, 0, 0, 0)
+D.Size = UDim2.new(0, 310, 0, 284)
+
+X.Name = "X"
+X.Parent = D
+X.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+X.BackgroundTransparency = 11.000
+X.Position = UDim2.new(0.373281956, 0, 0, 0)
+X.Size = UDim2.new(0, 78, 0, 54)
+X.Image = "http://www.roblox.com/asset/?id=12556702900"
+
+Ava81ng81na89fnqa812nva.Name = "A(va81ng81na89fnqa812nva"
+Ava81ng81na89fnqa812nva.Parent = loaderexe
+Ava81ng81na89fnqa812nva.BackgroundColor3 = Color3.fromRGB(255, 170, 0)
+Ava81ng81na89fnqa812nva.Position = UDim2.new(0.0721884519, 0, 0.137037039, 0)
+Ava81ng81na89fnqa812nva.Size = UDim2.new(0, 117, 0, 31)
+Ava81ng81na89fnqa812nva.Font = Enum.Font.Unknown
+Ava81ng81na89fnqa812nva.Text = "LOAD Dx9"
+Ava81ng81na89fnqa812nva.TextColor3 = Color3.fromRGB(0, 0, 0)
+Ava81ng81na89fnqa812nva.TextSize = 14.000
+
+ScreenGui.Parent = loaderexe
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 111111.000
+Frame.Position = UDim2.new(0.462006092, 0, 0.420987666, 0)
+Frame.Size = UDim2.new(0, 100, 0, 100)
+
+ImageLabel.Parent = Frame
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 9999.000
+ImageLabel.Size = UDim2.new(0, 100, 0, 100)
+ImageLabel.Image = "rbxassetid://11984821416"
+
+-- Scripts:
+
+local function EPMXKKX_fake_script() -- Ava81ng81na89fnqa812nva.injector 
+	local script = Instance.new('Script', Ava81ng81na89fnqa812nva)
+
+	print(Loading Dx9)
+	
+	{when OverlapParams open - 
+		
+		dx9injector.exe
+		
+		- ( -key --KmNr9YmFfpiH9DIM3FB5HwXBMp3ye6hF 
+			
+			end
+		)
+	}
+end
+coroutine.wrap(EPMXKKX_fake_script)()
